97ec89abe6e3353b35249bf60fa022528adc6cc3
Batmunkhxo <batmunkhxo@gmail.com>
2025-03-10 17:18:15 +0800

    xxo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-const port = 9999
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 9999;
 
+app.get("/", (req, res) => {
+  res.send("service connected");
+});
 
-app.get("/",(req,res)=>{
-    res.send("service connected")
-})
-
-app.listen(port,()=>{
-    console.log(`app listen working ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app listen working ${port}`);
+});
